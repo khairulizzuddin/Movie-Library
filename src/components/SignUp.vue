0b5ce1b06af6637f89dab0531c43817a00c395d2
khairulizzuddin <khairulizzuddin05@gmail.com>
2024-08-27 17:28:25 +0800

    Initial commit

---
+++ src/components/SignUp.vue
@@ -0,0 +1,74 @@
+<template>
+    <img class="logo" src="../assets/Starbucks.svg.png" />
+    <h1>Sign Up</h1>
+    <div class="register">
+        <input type="text" v-model="name" placeholder="Enter Name"/>
+        <input type="email" v-model="email" placeholder="Enter Email"/>
+        <input type="password" v-model="password" placeholder="Enter Password"/>
+        <button v-on:click="signUp">Sign Up</button>
+        <p>
+            <router-link to="./login">Login</router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name:'SignUp',
+    data()
+    {
+        return {
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        async signUp()
+        {
+            let result = await axios.post("http://localhost:3000/user",{
+                name:this.name,
+                email:this.email,
+                password:this.password
+            });
+            console.warn(result);
+            if(result.status==201)
+            {
+                localStorage.setItem("user-info", JSON.stringify(result.data))
+                this.$router.push({name:'Home'})
+            }
+        }
+    },
+    mounted()
+    {
+        let user = localStorage.getItem("user-info");
+        if(user)
+        {
+            this.$router.push({name:'Home'})
+        }
+    }
+}
+</script>
+
+<style>
+.logo{
+    width: 100px
+}
+.register input{
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid green;
+}
+.register button{
+    background-color: rgb(0, 73, 0);
+    border: 1px solid rgb(0, 73, 0);
+    color: aliceblue;
+    cursor: pointer;
+}
+</style>
